ac4715d9f4e6f83e547ca9d8cb42c0e35b462dd1
Bozos2 <bozesoldo8@gmail.com>
2024-03-15 18:43:25 +0100

    modified remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
