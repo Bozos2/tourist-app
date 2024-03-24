526a9d44ec66557eec7f43eb45c1a4abc23dd0f1
Bozos2 <bozesoldo8@gmail.com>
2024-03-24 15:01:26 +0100

    added typewriter-effect for landing page

--- app/(public)/home/landing-page.tsx
+++ app/(public)/home/landing-page.tsx
@@ -1,17 +1,39 @@
+"use client";
+
 import WaveSvg from "@/assets/svgs/wave-svg";
 import { Button } from "@/components/ui/button";
+import TypewriterComponent from "typewriter-effect";
 
 export const LandingPage = () => {
   return (
-    <section className="flex min-h-[calc(100vh-64px)] flex-col justify-between pt-24 sm:pt-36 lg:pt-48">
-      <div className="mx-6 flex flex-col justify-between gap-10 font-poppins sm:mx-24 lg:h-72 lg:w-[700px] lg:gap-0 xl:mx-32">
-        <h1 className="text-4xl">
-          Discover and rate the best places in your{" "}
-          <span className="text-primary">Favourite Cities</span>
-        </h1>
-        <p className="text-xl">
-          Explore iconic museums, delightful restaurants, and hidden gems
-          recommended by traveler&apos;s like you.
+    <section className="flex min-h-[calc(100vh-64px)] flex-col justify-between pt-6">
+      <div className="mx-6 flex flex-col justify-between gap-8 pt-12 font-poppins sm:mx-24 sm:pt-36 lg:w-[740px] lg:pt-48 xl:mx-32">
+        <div>
+          <h1 className="inline text-4xl sm:text-5xl">
+            Embark on an Adventure to Discover the Best{" "}
+          </h1>
+          <span className="inline-block text-4xl text-primary sm:text-5xl">
+            <TypewriterComponent
+              options={{
+                strings: [
+                  "Countries.",
+                  "Cities.",
+                  "Museums.",
+                  "Parks.",
+                  "Restaurants.",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </div>
+        <p className="max-w-[600px] text-xl">
+          Our platform enables you to explore iconic museums, indulge in
+          delightful eateries, and unearth hidden gems recommended by fellow
+          travelers like yourself. Whether you&apos;re an adventurous explorer
+          or a seeker of leisurely escapes, we have something tailored for
+          everyone&apos;s taste.
         </p>
         <div className="flex flex-row gap-9">
           <Button
